4cc9f6b86695bbcc9e4866643dc147ca8537f6d5
Sara <hipolitosara36@gmail.com>
2025-10-28 22:27:15 -0600

    ejercicio dao

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
+# main.py
+
 from models.user import User
 from dao.factory import get_dao_from_config
 
 def main():
+    # El dao_type en config.json determinará si es SQLServer, MySQL, etc.
     dao = get_dao_from_config("config.json")
-
-    dao.add_user(User(None, "Luis Arturo", "luis@example.com"))
-    dao.add_user(User(None, "Armando", "armando@example.com"))
+    
+    # Llama a los métodos que el DAO de SQL Server ya implementó
+    dao.add_user(User(None, "Sarahi H", "saracruz@example.com"))
+    dao.add_user(User(None, "Armando M", "armando99@example.com"))
+    dao.add_user(User(None, "Noemi S", "noemi23@example.com"))
+    dao.add_user(User(None, "Evelyn A", "aide55@example.com"))
+    
 
     print("Usuarios almacenados:")
     for u in dao.get_all_users():
-        print(u)
+        print(u) # Asumo que tu clase User tiene un buen método __str__ o __repr__
 
 if __name__ == "__main__":
     main()
